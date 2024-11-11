0d7fe918011fe361e1225d9849a9f260668168e4
Spiderlight6 <170469796+Spiderlight6@users.noreply.github.com>
2024-11-11 14:36:06 +0100

    modified:   index.js

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 
-// create a constructor to that contain books
-function Books(title,author,pages, read){
+const books = [];
+
+
+// create a constructor to that create books
+function Book(title,author,pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 
     this.info =  function(){
-        return `${this.title} by ${author}, ${pages} pages, ${read}`;
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
     };
 
 };
+
+
+const theGiver = new Book("The Giver","Lois Lowry",179, true);
+
+function addBookToLibrary(bookObject){
+    books.push(bookObject);
+}
+
+
+addBookToLibrary(theGiver);
+
 
