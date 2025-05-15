e0a71db9334e9b0eca2fb8db9405d30dcbdb2515
Spiderlight6 <170469796+Spiderlight6@users.noreply.github.com>
2025-05-15 20:01:48 +0200

    new file:   image placeholder.jpeg 	modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const books = [];
+const myLibrary = [];
 
 
 // create a constructor to that create books
@@ -8,6 +8,7 @@ function Book(title,author,pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
+    
 
     this.info =  function(){
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
@@ -15,22 +16,39 @@ function Book(title,author,pages, read){
 
 };
 
-
+/*
 const theGiver = new Book("The Giver","Lois Lowry",179, true);
 const ofMiceAndMen = new Book("of Mice and Men","John Steinbeck",144, true);
 const bornACrime = new Book("Born a Crime","Trevor Noah",304, true);
 const brothersKaramazov = new Book("brothers karamazov","Fyodor Dostoevsky",1056, false);
+*/
 
-function addBookToLibrary(bookObject){
-    books.push(bookObject);
+
+function addBookToLibrary(title,author,pages,read){
+    let bookName = new Book(`${title}`,`${author}`,`${pages}`,`${read}`);
+    
+    myLibrary.push(bookName);
 }
 
 
-addBookToLibrary(theGiver);
-addBookToLibrary(ofMiceAndMen);
-addBookToLibrary(bornACrime);
-addBookToLibrary(brothersKaramazov);
+addBookToLibrary("The Giver","Lois Lowry",179,true);
+addBookToLibrary("of Mice And Men","Jhon Steinbeck",144,true);
+addBookToLibrary("born a Crime","Trevor Noah",304,true);
+addBookToLibrary("brothers Karamazov","Fyodor Dostoevsky",1056,false);
 
-books.forEach((book) =>{
-   console.log(book.info());
-});
+
+// wirte a function that does below 
+const bookTitle = document.getElementById("title")
+bookTitle.textContent = `${myLibrary[0].title}`
+
+const bookAuthor = document.getElementById("author")
+bookAuthor.textContent = `${myLibrary[0].author}`
+
+const bookPages = document.getElementById("pages")
+bookPages.textContent = `${myLibrary[0].pages}`
+
+// create a functio to add a book 
+
+function addBook(){
+    
+}
