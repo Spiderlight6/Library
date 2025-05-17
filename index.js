235d1a6b3a158fd96f7d097800465ba2c2e9f9e3
Spiderlight6 <170469796+Spiderlight6@users.noreply.github.com>
2025-05-18 01:02:13 +0200

    modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -7,48 +7,86 @@ function Book(title,author,pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.read = read === true || read === "true";
+
+    this.id = crypto.randomUUID();
     
-
-    this.info =  function(){
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-    };
-
 };
 
-/*
-const theGiver = new Book("The Giver","Lois Lowry",179, true);
-const ofMiceAndMen = new Book("of Mice and Men","John Steinbeck",144, true);
-const bornACrime = new Book("Born a Crime","Trevor Noah",304, true);
-const brothersKaramazov = new Book("brothers karamazov","Fyodor Dostoevsky",1056, false);
-*/
-
-
+// function to create a book 
 function addBookToLibrary(title,author,pages,read){
-    let bookName = new Book(`${title}`,`${author}`,`${pages}`,`${read}`);
-    
-    myLibrary.push(bookName);
+    let book = new Book(`${title}`,`${author}`,`${pages}`,`${read}`);
+
+    // add the book into an array
+    myLibrary.push(book);
 }
 
-
+// manually adding books to display 
 addBookToLibrary("The Giver","Lois Lowry",179,true);
 addBookToLibrary("of Mice And Men","Jhon Steinbeck",144,true);
 addBookToLibrary("born a Crime","Trevor Noah",304,true);
 addBookToLibrary("brothers Karamazov","Fyodor Dostoevsky",1056,false);
 
 
-// wirte a function that does below 
-const bookTitle = document.getElementById("title")
-bookTitle.textContent = `${myLibrary[0].title}`
 
-const bookAuthor = document.getElementById("author")
-bookAuthor.textContent = `${myLibrary[0].author}`
-
-const bookPages = document.getElementById("pages")
-bookPages.textContent = `${myLibrary[0].pages}`
-
-// create a functio to add a book 
-
-function addBook(){
+function displayBooks(array) {
+    const container = document.getElementById("container");
+    container.textContent = "";
     
-}
+    array.forEach(book => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        
+        let readStatus;
+        if (book.read) {
+           readStatus = "Yes";
+
+        } else {
+            readStatus = "No";
+        }
+        
+       const cardText = "Title: " + book.title + "\n" +
+                        "Author: " + book.author + "\n" +
+                        "Pages: " + book.pages + "\n" + 
+                        "Read it? " + readStatus;
+
+    
+        card.textContent = cardText;
+        
+        container.appendChild(card); 
+        
+        
+    });
+};
+
+displayBooks(myLibrary);
+
+
+// create a dialog to add book
+const dialog = document.querySelector("dialog");
+const openDialog = document.getElementById("openBtn");
+const closeDialog = document.getElementById("closeBtn");
+
+openDialog.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+closeDialog.addEventListener("click", () =>{
+    dialog.close();
+});
+
+
+const form = document.getElementById("form");
+form.addEventListener("submit",(event)=> {
+        event.preventDefault();
+        const titleValue = document.getElementById("title").value;
+        const authorValue = document.getElementById("author").value;
+        const pagesValue = document.getElementById("pages").value;
+        const readValue = document.getElementById("read-or-not").checked;
+        
+        addBookToLibrary(`${titleValue}`,`${authorValue}`,pagesValue,readValue);        
+        displayBooks(myLibrary);
+        dialog.close();
+});
+
+
